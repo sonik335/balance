51d4af1448cfd6a45470911ce622780ca675cdc9
Kirill <sonik3000.83@gmail.com>
2022-07-17 12:35:37 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int x = 200; // баланс
+        int balans = 191; // баланс
 
-        int s = 1050; // пополнение баланса в рублях
+        int replenishment = 1350; // пополнение баланса в рублях
 
-        int y = 100; // Стоимость 1-го бонусного рубля
+        int bonusValue = 100; // Стоимость 1-го бонусного рубля
 
         int bonus;
 
 
-        if (s > 1000) {
+        if ( replenishment > 1000) {
 
-            bonus = (s / y);
+            bonus = ( replenishment / bonusValue);
 
-            System.out.println((s / y) + " бонусов ");
-            System.out.println(bonus + (x + s) + " баланс ");
+            System.out.println(( replenishment / bonusValue) + " бонусов ");
+            System.out.println(bonus + (balans + replenishment ) + " баланс ");
         } else {
 
-            System.out.println(" нет бонусов " + (x + s) + " баланс ");
+            System.out.println(" нет бонусов " + (balans + replenishment) + " баланс ");
         }
 
     }
